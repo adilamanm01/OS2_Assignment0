1c8de468c8faa6ec2667d59195c5084d559fc208
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-01-24 00:57:26 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
-#include <stdio.h>
-#include <sys/types.h>
-int main()
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+int main(int argc, int argv) 
 {
-    fork();
-    fork();
-    fork();
-    printf("hello\n");
-    return 0;
-}
+    int n,max=200,i,c=1;
+    int a[100];
+
+   for(i=0;i<200;i++)
+   {
+    if(n<0)
+    printf("invalid entry");
+    else
+    {
+        if(n%2==0)
+        {
+            n=n/2;
+            scanf("%d",&a[i]);
+            n=a[i];
+            c=c+1;
+        }
+        else{
+                n=(3*n)+1;
+                scanf("%d",&a[i]);
+                n=a[i];
+                c=c+1;
+        }
+
+    }
+   }
+   for(i=0;i<c;i++)
+   printf(" %d ",a[i]);
+   return 0;
+} 
+
