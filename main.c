3743316e5342ce6cbfd277a297f42e40c5f6b80d
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-01-29 00:21:13 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+/*
+Name : Adil Aman Mohammed 
+CWID : A20395630
+Description : The following code is an implimentation of fork() to create 2 child's and 1 parent for solving collatz Conjecture
+*/
+
+//Declaring Required libraries 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -5,56 +12,81 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+
+//Declaration of function
 void function(int N,int c)
 {
+  //Declaration of char type variable to store the string
     char buffer[50];
-    
+    //using sprintf to store the combined string in to a char variable
     sprintf(buffer, "From Child %d init n = %d, ",c,N);
+    //displaying the buffer usinf printf
     printf("%s", buffer);
+  //while loop for running the loop till n is less than 1
   while(N > 1){
-    if(N % 2 == 0)
+    //logic of collatz conjecture
+    if(N % 2 == 0) // logic for even numbers
     { 
       N = N / 2;
+      //using sprintf to store the combined string in to a char variable
       sprintf(buffer, "From Child %d n = %d, ",c,N);
+      //displaying the buffer usinf printf
       printf("%s", buffer);
     }
-    else{ //for odd numbers
+    else{ //logic for odd numbers
       N = N * 3 + 1;
+      //using sprintf to store the combined string in to a char variable
       sprintf(buffer, "From Child %d n = %d, ",c,N);
+      //displaying the buffer usinf printf
       printf("%s", buffer);
     }
   }
+  //printing in new line
   printf("\n");
 }
 
 
-// Driver code
+//Declaration of main function with argc and argv for command line input
 int main(int argc, char* argv[])
 {
-	int pid, pid1, pid2;
+  //declaring variables
+	int pid, pid1;
+  //converting a char type to integer type
 	int x = atoi(argv[1]);
-
+  //calling fork()
 	pid = fork();
-
-	// If fork() returns zero then it
+	//condition for input number limit
+  if(x>0 || x<40)
+  {
+    // If fork() returns zero then it
 	// means it is child process.
 	if (pid == 0) {
-            
-            if(x>0 || x<40)
+    //child 1
+    //calling function
             function(x,1);
 	}
 
 	else {
+    //using wait() for child 1 to complete the process
+    wait(0);
+    //calling fork()
 		pid1 = fork();
 		if (pid1 == 0) {
+      //child 2
+      //calling function with a condition x+3
 			function(x+3,2);
 		}
 		else {
+      //parent
+      //using sleep() for delay
+      sleep(1);
+      //using wait() for children to complete the process
 			   wait(0);
 				printf("Children Completed\n");
 			
 		}
 	}
+  }
 
 	return 0;
 }
